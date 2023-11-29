4f8e630e89d300856cddc67c9eb9226425095580
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2023-11-29 06:07:29 +0000

    Completed

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+#Ruby script that accepts one argument and pass it to a regular expression matching method
+#The regex should not contain square brackets
+puts ARGV[0].scan(/hbt*n/).join
