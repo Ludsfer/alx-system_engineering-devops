f40229ef188a88a4904692c985e2ce2b3a1b8a7a
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-03-06 20:48:01 +0000

    Completed

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that returns the number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns 0 if not a valid subreddit, else
+    returnsthe number of subscribers
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
