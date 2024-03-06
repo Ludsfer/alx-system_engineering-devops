2dea61329a6df92fe89f6eae73729be33b1abd14
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-03-06 17:07:17 +0000

    Completed

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Changed `phpp` extensions in the WordPress file `wp-settings.php` to `php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
